6aa45ef1b7450b7869ef1306a064e24475460481
MUBASHIR EK <119831619+mubashir-edk@users.noreply.github.com>
2024-02-06 17:31:39 +0530

    Update signals.py

--- water_purifier/purifier/signals.py
+++ water_purifier/purifier/signals.py
@@ -1,16 +1,16 @@
 # from models import *
 # from twilio.rest import Client
 
-# account_sid = 'ACc65d2d8098ee2f4216eb66fcd9580dba'
-# auth_token = '844937db2ff91d18d7c71cd0a754b807'
+# account_sid = '......'
+# auth_token = '......'
 # client = Client(account_sid, auth_token)
 
 # for service in ServiceWork.objects.all():
 
 #     message = client.messages.create(
 #         body=f'Hi {service.customer_code.name}, tomorrow {service.service_date} is your scheduled service date.',
-#         from_='+12018904884',
-#         to='+919526928433'
+#         from_='',
+#         to=''
 #     )
 
 # print(message.sid)
